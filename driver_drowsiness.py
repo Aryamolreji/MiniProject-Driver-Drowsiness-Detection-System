07e2c677f9d120025f40b7ee0d0e7337fa210055
Aryamolreji <77604024+Aryamolreji@users.noreply.github.com>
2022-01-17 21:51:35 +0530

    Checking if eyes are blinked

--- driver_drowsiness.py
+++ driver_drowsiness.py
@@ -25,3 +25,15 @@ def blinked(a,b,c,d,e,f):
 	up = compute(b,d) + compute(c,e)
 	down = compute(a,f)
 	ratio = up/(2.0*down)
+#Checking if it is blinked
+	if(ratio>0.25):
+		return 2
+	elif(ratio>0.21 and ratio<=0.25):
+		return 1
+	else:
+		return 0
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
