01217be22c6e11b1ffdb8b4fbfd097dfa3ec9cd9
Aryamolreji <77604024+Aryamolreji@users.noreply.github.com>
2022-01-17 21:35:07 +0530

    #status marking for current state

--- driver_drowsiness.py
+++ driver_drowsiness.py
@@ -12,3 +12,9 @@ cap = cv2.VideoCapture(0)
 #Initializing the face detector and landmark detector
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+#status marking for current state
+sleep = 0
+drowsy = 0
+active = 0
+status=""
+color=(0,0,0)
