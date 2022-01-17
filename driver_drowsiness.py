8925d1b49869731a84e6f52a7c7f319fb0c1186a
Abhijith6789 <77606421+Abhijith6789@users.noreply.github.com>
2022-01-17 21:32:24 +0530

    Dectector and predictor
    Frontal face and landmark

--- driver_drowsiness.py
+++ driver_drowsiness.py
@@ -9,3 +9,6 @@ from imutils import face_utils
 #checking
 #Initializing the camera and taking the instance
 cap = cv2.VideoCapture(0)
+#Initializing the face detector and landmark detector
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
