ada3604eaebb6072772381384e32d21d4d8d6aeb
Abhijith6789 <77606421+Abhijith6789@users.noreply.github.com>
2022-02-11 15:32:37 +0530

    buttons

--- driver_drowsiness.py
+++ driver_drowsiness.py
@@ -99,7 +99,8 @@ a.title("DRIVER DROWSINESS DETECTION SYSTEM")
 a.geometry("550x630+300+100")
 a.configure(background="#f4ab79")
 Label(a,text="\nDRIVER DROWSINESS DETECTION SYSTEM",font="Tw-Cen-MT-Condensed-Extra-Bold 18 bold",bg="#f4ab79").pack()
-
+Button(a,text="ACTIVATE",height=3,font="Bahnschrift 14 bold",width=20,border=0,command=activate,background="#b57e58",activebackground="black", activeforeground="red",highlightcolor="black").place(x=165,y=180)
+Button(a,text="QUIT",height=2,font="Bahnschrift 14 bold",width=10,command=exit,border=0,background="#c12b2b",activebackground="black", activeforeground="white",highlightcolor="black").place(x=225,y=400)
 
 
 
